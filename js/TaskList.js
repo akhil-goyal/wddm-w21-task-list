9adec404c3e3229585efe9ab1c53c989893e71f1
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-02-16 16:00:42 -0500

    Implemented Add new todo by creating new class.

--- js/TaskList.js
+++ js/TaskList.js
@@ -4,7 +4,6 @@ import TaskItem from './TaskItem.js'
 const loadTaskView = ({id, complete, task}) => {
   return `<task-item data-id="${id}" data-complete="${complete}" data-task="${task}"></task-item>`
 }
-
 // A function that will build an entire list based on an Array of data
 export const renderList = (taskAr) => {
   //document.getElementById(`tasks`).innerHTML = taskAr.map(loadTaskView).join('\n')
@@ -18,7 +17,7 @@ export default class TaskList extends HTMLElement {
   constructor(todoList) {
     super()
 
-    this.root = this.attachShadow({ mode: `open` })
+    this.root = this.attachShadow({mode: `open`})
 
     const eleStyle = document.createElement(`style`)
     const txtStyle = document.createTextNode(`
@@ -29,44 +28,20 @@ export default class TaskList extends HTMLElement {
     eleStyle.appendChild(txtStyle)
     this.root.appendChild(eleStyle)
 
-    const userForm = document.createElement('form');
-
-    userForm.setAttribute('name', 'newTask');
-
-    const userInput = document.createElement('input');
-
-    userInput.setAttribute('type', 'text');
-    userInput.setAttribute('name', 'taskname');
-
-    const buttonSubmit = document.createElement('button');
-    buttonSubmit.setAttribute('type', 'submit');
-    buttonSubmit.innerHTML = 'Add';
-
-    buttonSubmit.addEventListener('click', event => {
-      this.addTaskTop(userInput.value);
-      event.preventDefault();
-    })
-
-    userForm.appendChild(userInput);
-    userForm.appendChild(buttonSubmit);
-
-
     // Build an empty list
     this.list = document.createElement(`ul`)
 
-    /*    // Add all of the <task-item> elements to the empty list
-        const loadTaskView = ({id, complete, task}) => {
-          return `<task-item data-id="${id}" data-complete="${complete}" data-task="${task}"></task-item>`
-        }
-        eleList.innerHTML = todoList.map(loadTaskView).join(`\n`)
-    */
+/*    // Add all of the <task-item> elements to the empty list
+    const loadTaskView = ({id, complete, task}) => {
+      return `<task-item data-id="${id}" data-complete="${complete}" data-task="${task}"></task-item>`
+    }
+    eleList.innerHTML = todoList.map(loadTaskView).join(`\n`)
+*/
     todoList.forEach((item) => {
       const aTask = new TaskItem(item)
-      aTask.addEventListener('taskChanged', event => { console.log('Task Complete?', aTask.complete) })
+      aTask.addEventListener('taskChanged', event => {console.log('Task Complete?', aTask.complete)})
       this.list.appendChild(aTask)
     })
-
-    this.root.appendChild(userForm);
 
     this.root.appendChild(this.list)
 
@@ -74,15 +49,27 @@ export default class TaskList extends HTMLElement {
 
   addNewTask(name) {
     // Figure out what the next available id is
-    const aTask = new TaskItem({ id: 4, task: name, complete: false })
-    aTask.addEventListener('taskChanged', event => { console.log('Task Complete?', aTask.complete) })
+    const aTask = new TaskItem({id:4, task:name, complete:false})
+    aTask.addEventListener('taskChanged', event => {console.log('Task Complete?', aTask.complete)})
     this.list.appendChild(aTask)
   }
 
-  addTaskTop(name) {
-    const aTask = new TaskItem({ id: 4, task: name, complete: false })
-    aTask.addEventListener('taskChanged', event => { console.log('Task Complete?', aTask.complete) })
-    this.list.insertBefore(aTask, this.list.childNodes[0]);
+  render(newTaskList) {
+    // rebuild the list!!!
+    // - empty the list
+    // - loop through the data to rebuild
+    
+    console.log(this.list.children.length)
+    this.list.innerHTML = ''
+    console.log(this.list.children.length)
+
+    newTaskList.forEach((item) => {
+      const aTask = new TaskItem(item)
+      aTask.addEventListener('taskChanged', event => {console.log('Task Complete?', aTask.complete)})
+      this.list.appendChild(aTask)
+    })
+
+    console.log(this.list.children.length)
   }
 
 }
--- js/fetch.js
+++ js/fetch.js
@@ -6,19 +6,30 @@ const todoList = [
   }, {
     "id": 1234,
     "task": "Order groceries",
-    "complete": true 
+    "complete": true
   }, {
     "id": 3,
     "task": "Learn about Web Components",
-    "complete": false 
+    "complete": false
   }
 ]
-
-
 
 export const getJsonData = (url) => {
   // Pretend we fetched this data
   // fetch(url, blah blah blah)
 
+  return todoList
+}
+
+export const addNewTask = (taskName) => {
+  // Pretend we are updating the database
+  // POST request
+  todoList.push({
+    "id": todoList.length + 1,
+    "task": taskName,
+    "complete": false
+  })
+
+  // Pretend this came from the database after a successful update
   return todoList
 }
